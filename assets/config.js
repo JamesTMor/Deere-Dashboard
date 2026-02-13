af3aa3f5d62ea78144da84626c1f946bccbbfb2c
JamesTMor <j-morgan2@wiu.edu>
2026-02-13 15:10:09 -0600

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+// 🔧 Set these to your repo coordinates
+window.DASHBOARD_CONFIG = {
+  repoOwner: "YOUR_GITHUB_USERNAME_OR_ORG",
+  repoName: "YOUR_REPO_NAME",
+  defaultStatus: "All" // Open | In Progress | Done | All
+};
